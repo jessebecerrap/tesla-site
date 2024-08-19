b10cae4e61919de4a6f460b9e6be116b83e996cd
Jesse Becerra <jessebecerrap@gmail.com>
2024-08-19 01:18:09 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,astro}"],
+  theme: {
+    fontFamily: {
+			sans: ['Noto Sans', 'sans-serif']
+		},
+    extend: {},
+  },
+  plugins: [],
+}
+
